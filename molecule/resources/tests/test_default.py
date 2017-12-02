2bf5c88b0ebc9c06a76d1fc16b5e44105acf7e85
Matthew Plachter <matthew.plachter@gmail.com>
2017-12-02 18:13:10 -0500

    Fixed systemd access in ubuntu container, turned yamllint back on and fixed lint issues, also added a few more configuration tests for testinfra

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -1,8 +1,8 @@
 import testinfra.utils.ansible_runner
-
+import pytest
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('all')
+    '../default/.molecule/ansible_inventory.yml').get_hosts('all')
 
 
 def test_kapacitor_running_and_enabled(Service):
@@ -11,12 +11,30 @@ def test_kapacitor_running_and_enabled(Service):
     assert kapacitor.is_enabled
 
 
-def test_kapacitor_config(File):
+@pytest.mark.parametrize("teststring", [
+    ('test_db = \\["rp_test_db"\\]'),
+    ('test_db_2 = \\["rp_test_db_one", "rp_test_db_two"\\]'),
+    ('https-certificate = "/etc/ssl/kapacitor.pem"'),
+    ('log-enabled = true'),
+    ('write-tracing = false'),
+    ('pprof-enabled = false'),
+    ('ttps-enabled = false'),
+    ('stats-interval = "10s"'),
+    ('database = "_kapacitor"'),
+    ('retention-policy= "default"'),
+    ('url = "https://usage.influxdata.com"'),
+    ('dir = "/var/lib/kapacitor/replay"'),
+    ('dir = "/var/lib/kapacitor/tasks"'),
+    ('snapshot-interval = "60s"'),
+    ('boltdb = "/var/lib/kapacitor/kapacitor.db"'),
+    ('file = "/var/log/kapacitor/kapacitor.log"'),
+    ('level = "INFO"'),
+    ('urls = \\["http://localhost:8086"\\]')
+])
+def test_kapacitor_config(File, teststring):
     kap_config = File("/etc/kapacitor/kapacitor.conf")
     assert kap_config.exists
-    assert kap_config.contains('test_db = \\["rp_test_db"\\]')
-    assert kap_config.contains('test_db_2 = ' +
-                               '\\["rp_test_db_one", "rp_test_db_two"\\]')
+    assert kap_config.contains(teststring)
 
 
 def test_tick_file(File):
